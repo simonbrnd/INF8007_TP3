e58136d703e5c8864117c21ecd2fe34a52e054bc
simonbrnd <simon.bernard1@eleves.enpc.fr>
2022-03-24 21:49:35 -0400

    Nouvelle façon de router mais ça ne fonctionne toujours pas

--- Base/app.py
+++ Base/app.py
@@ -1,7 +1,8 @@
 from re import I
 import pandas as pd
 from flask import Flask, render_template, request, redirect
-from datetime import datetime
+from datetime import datetime, timedelta
+import time
 import json
 
 app = Flask(__name__)
@@ -22,7 +23,18 @@ def compute_vars_for_rending_template() :
     pdq = pd.read_csv(PDQ_FILE_PATH, sep=";")
     categorie = pd.read_csv(CATEGORIE_FILE_PATH, sep=";")
     qdt = pd.read_csv(QDT_FILE_PATH, sep=";")
+
+    current_datetime = datetime.now()
+    current_hour_in_seconds = current_datetime.hour*3600 + current_datetime.minute*60 + current_datetime.second
+    if current_hour_in_seconds >= 60  and current_hour_in_seconds <= 28859 :
+        current_quart = "nuit"
+    elif current_hour_in_seconds <= 59  or current_hour_in_seconds >= 57660 :
+        current_quart = "soir"
+    else :
+        current_quart = "jour"
     
+    print("Current quart :", current_quart)
+
     nbInterventionsDf = interventions.drop(columns=["DATE_INCIDENT", "CATÉGORIE", "QUART_TRAVAIL"]).groupby("PDQ").count()
     nbInterventionsDf = nbInterventionsDf.rename(columns={'ID_INTERVENTION':'NB_INTERVENTION'})
     nbInterventionsParPDQ = nbInterventionsDf.to_dict()
@@ -33,9 +45,7 @@ def compute_vars_for_rending_template() :
     catIntervention = json.dumps(categorie.set_index("LIBELLÉ").squeeze().to_dict(),ensure_ascii=False)
     emplacementsPDQ = json.dumps(pdq.set_index("PDQ").squeeze().to_dict(),ensure_ascii=False)
 
-    lastId = interventions["ID_INTERVENTION"].values[-1]
-
-    return (interventionsParPoste, nbInterventionsParPDQ, DateMin, DateMax, PDQ, categorie, qdt, catIntervention, emplacementsPDQ, lastId)
+    return (interventionsParPoste, nbInterventionsParPDQ, DateMin, DateMax, PDQ, categorie, qdt, catIntervention, emplacementsPDQ, current_quart)
 
 
 def valid_intervention_number(id, interventions_pd) :
@@ -48,8 +58,9 @@ def valid_intervention_number(id, interventions_pd) :
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    print("Index function called")
     # Initialisation
-    (interventionsParPoste, nbInterventionsParPDQ, DateMin, DateMax, PDQ, categorie, qdt, catIntervention, emplacementsPDQ, lastId) = compute_vars_for_rending_template()
+    (interventionsParPoste, nbInterventionsParPDQ, DateMin, DateMax, PDQ, categorie, qdt, catIntervention, emplacementsPDQ, current_quart) = compute_vars_for_rending_template()
     return render_template('Base_TP3.html', 
                             interventionsparPoste=interventionsParPoste, 
                             nbInterventionsParPDQ=nbInterventionsParPDQ,
@@ -60,49 +71,31 @@ def index():
                             Quart=qdt,
                             catIntervention=catIntervention,
                             emplacementsPDQ=emplacementsPDQ,
-                            lastId=lastId,
                             invalid = None,
                             val=json.dumps(False),
                             modifyNoIntervention=json.dumps(None),
-                            modifyInfos=None)
+                            modifyInfos=None,
+                            currentQuart = current_quart)
 
 
-@app.route("/add", methods=['POST', 'GET']) 
-def add() :
-    # Méthode pour ajouter un rapport, compléter les données et afficher une pop-up
-    if request.method == 'POST' and len(request.form) > 0 :
+@app.route("/?add_pdq_nb=<int:pdq_id>&add_cat_intervention=<string:cat>&add_date_incident=<string:intervention_date>&add_quart=<string:quart>&add-new-intervention=#nav-add", methods=['GET']) 
+def add(pdq_id, cat, intervention_date, quart) :
+    print("Into the add method !")
+    # Méthode pour ajouter un rapport, compléter les données et renvoyer vers le template pour afficher la pop-up
+    if request.method == 'GET' :
+        print("GET !")
         interventions_file = open(file=INTERVENTIONS_FILE_PATH, mode='r', encoding='UTF-8')
         last_line = interventions_file.readlines()[-1]
         new_id = int(last_line.split("\t")[0]) + 1
         print("new id :", new_id)
         interventions_file = open(file=INTERVENTIONS_FILE_PATH, mode='a', encoding='UTF-8')
-        interventions_file.write(f"{new_id}\t{request.form['add_date_incident']}\t{request.form['add_cat_intervention']}\t{request.form['add_pdq_nb']}\t{request.form['add_quart']}\n")
+        interventions_file.write(f"{new_id}\t{intervention_date}\t{cat}\t{pdq_id}\t{quart}\n")
         interventions_file.close()
         print("written")
-
-        (interventionsParPoste, nbInterventionsParPDQ, DateMin, DateMax, PDQ, categorie, qdt, catIntervention, emplacementsPDQ, lastId) = compute_vars_for_rending_template()
-
-        return render_template("add.html",
-                        interventionsparPoste=interventionsParPoste, 
-                        nbInterventionsParPDQ=nbInterventionsParPDQ,
-                        DateMin=DateMin,
-                        DateMax=DateMax,
-                        PDQ=PDQ,
-                        cat=categorie,
-                        Quart=qdt,
-                        catIntervention=catIntervention,
-                        emplacementsPDQ=emplacementsPDQ,
-                        lastId=lastId,
-                        invalid=None,
-                        validModify=json.dumps(False),
-                        modifyNoIntervention=None,
-                        modifyInfos=None)
-
-    return redirect("/#nav-add")
+        return render_template("add.html", lastId=new_id)
 
 
-
-@app.route("/modify", methods=['POST'])
+"""@app.route("/modify", methods=['POST'])
 def modify() :
     if request.method == 'POST' and len(request.form) > 0 :
         id_to_modify = request.form['modify_no_intervention']
@@ -124,7 +117,7 @@ def modify() :
             modifyNoIntervention = None
             line = None
 
-        (interventionsParPoste, nbInterventionsParPDQ, DateMin, DateMax, PDQ, categorie, qdt, catIntervention, emplacementsPDQ, lastId) = compute_vars_for_rending_template()
+        (interventionsParPoste, nbInterventionsParPDQ, DateMin, DateMax, PDQ, categorie, qdt, catIntervention, emplacementsPDQ) = compute_vars_for_rending_template()
 
         return render_template(file,
                                 interventionsparPoste=interventionsParPoste, 
@@ -136,12 +129,11 @@ def modify() :
                                 Quart=qdt,
                                 catIntervention=catIntervention,
                                 emplacementsPDQ=emplacementsPDQ,
-                                lastId=lastId,
                                 invalid=id_to_modify,
                                 validModify=json.dumps(validModify),
                                 modifyNoIntervention=json.dumps(modifyNoIntervention),
                                 modifyInfos=line)
-
+"""
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5555, debug=False)
